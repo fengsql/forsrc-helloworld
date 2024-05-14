471aa7e4df8128a93e67e51e0dad283056356eaa
michael <fengsql@126.com>
2024-05-14 14:02:06 +0800

    forsrc 生成的 helloworld 演示项目

--- src/main/java/com/example/mvc/event/after/AfterGoods.java
+++ src/main/java/com/example/mvc/event/after/AfterGoods.java
@@ -1,18 +1,12 @@
 package com.example.mvc.event.after;
 
-import com.forsrc.common.extend.bean.ParamExport;
-import com.example.mvc.bean.detail.DetailGoods;
-import com.example.mvc.bean.rep.RepGoods;
-import com.example.mvc.bean.req.ReqGoods;
 import com.example.mvc.event.base.BaseAfter;
 import com.example.mvc.model.Goods;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.Map;
-import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
